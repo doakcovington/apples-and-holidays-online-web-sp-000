27a578e07a1bd1be4d89ce062b2f6aa7f7598dd3
Doak Covington <doakcovington@protonmail.com>
2020-01-14 22:51:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,14 +48,16 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   winter_supplies = []
-    holiday_hash.each do |season,holiday|
-      if season == :winter
-        holiday.each do |key,value|
-          winter_supplies << value
+  holiday_hash.each do |season,holiday|
+    if season == :winter
+      holiday.each do |key,value|
+        value.each do |supply|
+          winter_supplies << supply
         end
       end
     end
-    winter_supplies
+  end
+  winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)

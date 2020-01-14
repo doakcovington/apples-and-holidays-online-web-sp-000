96ad1c710c2479cdcf58d05c71dce98066280f46
Doak Covington <doakcovington@protonmail.com>
2020-01-14 22:56:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s}:#{holiday.to_s}"
+  end
 
 end
 

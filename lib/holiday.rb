fa26c5108b70ef65e70e878bf58ce34e67413657
Doak Covington <doakcovington@protonmail.com>
2020-01-14 22:28:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_supplies = []
+    
+  winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)

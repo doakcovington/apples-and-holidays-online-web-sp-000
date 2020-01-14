fe77454f9a2ff1bc7fea9fb79e894a0132a7fd25
Doak Covington <doakcovington@protonmail.com>
2020-01-14 22:04:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,9 +41,10 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_hash[:season] = [:holiday_name]
-  holiday_hash[:season][:holiday_name] = supply_array.to_s
-  holiday_hash
+  holiday_hash.each do |key, value|
+    if key == season
+      value = holiday_name
+  end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
